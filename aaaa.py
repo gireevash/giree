fffad3fc4ffd3e596fc1c6e7c42d559f1498230e
gireevash <51222671+gireevash@users.noreply.github.com>
2019-07-19 12:25:15 +0530

    Create aaaa.py

---
+++ aaaa.py
@@ -0,0 +1,6 @@
+mon=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+tue=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+f=str(input())
+for v in range(20):
+    if(f==mon[v]):
+        print(tue[v])
